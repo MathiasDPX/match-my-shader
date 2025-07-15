05d02b0ce464ff23214022851e5cddb21ff8f594
MathiasDPX <mathias@dupeux.net>
2025-07-15 22:35:43 +0200

    add "What If?" challenge

---
+++ challenges/what_if.py
@@ -0,0 +1,14 @@
+HEADERS = {
+    "title": "What If?",
+    "starter": '# What if if-statment didnt exist ?\n\nreturn 0x00FF00',
+    "size": (16, 16),
+    "index": 3,
+
+    "_author": "@mathias"
+}
+
+def run(x,y):
+    if y >= 8:
+        return 0xFF0000
+    
+    return 0x0000FF
